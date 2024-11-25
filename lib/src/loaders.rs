8b6ecee26d0f14e139de16e8ae06018d2caa101e
Ifiok Jr. <ifiokotung@gmail.com>
2024-11-25 07:21:42 +0000

    feat!: rename `has_seeds` to `has_seeds_with_bump`
    Also add new `find_canonical_bump` with validates and ensures the
    provided account uses the correct seeds.

--- lib/src/loaders.rs
+++ lib/src/loaders.rs
@@ -1,147 +1,170 @@
 use bytemuck::Pod;
-use solana_program::{account_info::AccountInfo, program_error::ProgramError, pubkey::Pubkey};
+use solana_program::account_info::AccountInfo;
+use solana_program::program_error::ProgramError;
+use solana_program::pubkey::Pubkey;
 
-use crate::{
-    AccountDeserialize, AccountInfoValidation, AsAccount, CloseAccount, Discriminator,
-    LamportTransfer,
-};
+use crate::AccountDeserialize;
+use crate::AccountInfoValidation;
+use crate::AsAccount;
+use crate::CloseAccount;
+use crate::Discriminator;
+use crate::LamportTransfer;
 
 impl AccountInfoValidation for AccountInfo<'_> {
-    fn is_signer(&self) -> Result<&Self, ProgramError> {
-        if !self.is_signer {
-            return Err(ProgramError::MissingRequiredSignature);
-        }
+	fn is_signer(&self) -> Result<&Self, ProgramError> {
+		if !self.is_signer {
+			return Err(ProgramError::MissingRequiredSignature);
+		}
 
-        Ok(self)
-    }
+		Ok(self)
+	}
 
-    fn is_writable(&self) -> Result<&Self, ProgramError> {
-        if !self.is_writable {
-            return Err(ProgramError::MissingRequiredSignature);
-        }
+	fn is_writable(&self) -> Result<&Self, ProgramError> {
+		if !self.is_writable {
+			return Err(ProgramError::MissingRequiredSignature);
+		}
 
-        Ok(self)
-    }
+		Ok(self)
+	}
 
-    fn is_executable(&self) -> Result<&Self, ProgramError> {
-        if !self.executable {
-            return Err(ProgramError::InvalidAccountData);
-        }
+	fn is_executable(&self) -> Result<&Self, ProgramError> {
+		if !self.executable {
+			return Err(ProgramError::InvalidAccountData);
+		}
 
-        Ok(self)
-    }
+		Ok(self)
+	}
 
-    fn is_empty(&self) -> Result<&Self, ProgramError> {
-        if !self.data_is_empty() {
-            return Err(ProgramError::AccountAlreadyInitialized);
-        }
+	fn is_empty(&self) -> Result<&Self, ProgramError> {
+		if !self.data_is_empty() {
+			return Err(ProgramError::AccountAlreadyInitialized);
+		}
 
-        Ok(self)
-    }
+		Ok(self)
+	}
 
-    fn is_program(&self, program_id: &Pubkey) -> Result<&Self, ProgramError> {
-        self.has_address(program_id)?.is_executable()
-    }
+	fn is_program(&self, program_id: &Pubkey) -> Result<&Self, ProgramError> {
+		self.has_address(program_id)?.is_executable()
+	}
 
-    fn is_type<T: Discriminator>(&self, program_id: &Pubkey) -> Result<&Self, ProgramError> {
-        self.has_owner(program_id)?;
-        let data = self.try_borrow_data()?;
-        let data_len = 8 + std::mem::size_of::<T>();
+	fn is_type<T: Discriminator>(&self, program_id: &Pubkey) -> Result<&Self, ProgramError> {
+		self.has_owner(program_id)?;
+		let data = self.try_borrow_data()?;
+		let data_len = 8 + std::mem::size_of::<T>();
 
-        if data[0].ne(&T::discriminator()) {
-            return Err(ProgramError::InvalidAccountData);
-        }
+		if data[0].ne(&T::discriminator()) {
+			return Err(ProgramError::InvalidAccountData);
+		}
 
-        if data.len() != data_len {
-            return Err(ProgramError::AccountDataTooSmall);
-        }
+		if data.len() != data_len {
+			return Err(ProgramError::AccountDataTooSmall);
+		}
 
-        Ok(self)
-    }
+		Ok(self)
+	}
 
-    fn has_owner(&self, owner: &Pubkey) -> Result<&Self, ProgramError> {
-        if self.owner.ne(owner) {
-            return Err(ProgramError::InvalidAccountOwner);
-        }
+	fn is_sysvar(&self, sysvar_id: &Pubkey) -> Result<&Self, ProgramError> {
+		self.has_owner(&solana_program::sysvar::ID)?
+			.has_address(sysvar_id)
+	}
 
-        Ok(self)
-    }
+	fn has_owner(&self, owner: &Pubkey) -> Result<&Self, ProgramError> {
+		if self.owner.ne(owner) {
+			return Err(ProgramError::InvalidAccountOwner);
+		}
 
-    fn has_address(&self, address: &Pubkey) -> Result<&Self, ProgramError> {
-        if self.key.ne(&address) {
-            return Err(ProgramError::InvalidAccountData);
-        }
+		Ok(self)
+	}
 
-        Ok(self)
-    }
+	fn has_address(&self, address: &Pubkey) -> Result<&Self, ProgramError> {
+		if self.key.ne(&address) {
+			return Err(ProgramError::InvalidAccountData);
+		}
 
-    fn has_seeds(&self, seeds: &[&[u8]], program_id: &Pubkey) -> Result<&Self, ProgramError> {
-        let pda = Pubkey::find_program_address(seeds, program_id);
-        if self.key.ne(&pda.0) {
-            return Err(ProgramError::InvalidSeeds);
-        }
+		Ok(self)
+	}
 
-        Ok(self)
-    }
+	fn has_seeds_with_bump(
+		&self,
+		seeds: &[&[u8]],
+		program_id: &Pubkey,
+	) -> Result<&Self, ProgramError> {
+		let pda = Pubkey::create_program_address(seeds, program_id)?;
 
-    fn is_sysvar(&self, sysvar_id: &Pubkey) -> Result<&Self, ProgramError> {
-        self.has_owner(&solana_program::sysvar::ID)?
-            .has_address(sysvar_id)
-    }
+		if pda.ne(self.key) {
+			return Err(ProgramError::InvalidSeeds);
+		}
+
+		Ok(self)
+	}
+
+	fn find_canonical_bump(
+		&self,
+		seeds: &[&[u8]],
+		program_id: &Pubkey,
+	) -> Result<u8, ProgramError> {
+		let (key, bump) = Pubkey::find_program_address(seeds, program_id);
+
+		if key.ne(self.key) {
+			return Err(ProgramError::InvalidSeeds);
+		}
+
+		Ok(bump)
+	}
 }
 
 impl AsAccount for AccountInfo<'_> {
-    fn as_account<T>(&self, program_id: &Pubkey) -> Result<&T, ProgramError>
-    where
-        T: AccountDeserialize + Discriminator + Pod,
-    {
-        unsafe {
-            self.has_owner(program_id)?;
-            T::try_from_bytes(std::slice::from_raw_parts(
-                self.try_borrow_data()?.as_ptr(),
-                8 + std::mem::size_of::<T>(),
-            ))
-        }
-    }
+	fn as_account<T>(&self, program_id: &Pubkey) -> Result<&T, ProgramError>
+	where
+		T: AccountDeserialize + Discriminator + Pod,
+	{
+		unsafe {
+			self.has_owner(program_id)?;
+			T::try_from_bytes(std::slice::from_raw_parts(
+				self.try_borrow_data()?.as_ptr(),
+				8 + std::mem::size_of::<T>(),
+			))
+		}
+	}
 
-    fn as_account_mut<T>(&self, program_id: &Pubkey) -> Result<&mut T, ProgramError>
-    where
-        T: AccountDeserialize + Discriminator + Pod,
-    {
-        unsafe {
-            self.has_owner(program_id)?;
-            T::try_from_bytes_mut(std::slice::from_raw_parts_mut(
-                self.try_borrow_mut_data()?.as_mut_ptr(),
-                8 + std::mem::size_of::<T>(),
-            ))
-        }
-    }
+	fn as_account_mut<T>(&self, program_id: &Pubkey) -> Result<&mut T, ProgramError>
+	where
+		T: AccountDeserialize + Discriminator + Pod,
+	{
+		unsafe {
+			self.has_owner(program_id)?;
+			T::try_from_bytes_mut(std::slice::from_raw_parts_mut(
+				self.try_borrow_mut_data()?.as_mut_ptr(),
+				8 + std::mem::size_of::<T>(),
+			))
+		}
+	}
 }
 
 impl<'a, 'info> LamportTransfer<'a, 'info> for AccountInfo<'info> {
-    #[inline(always)]
-    fn send(&'a self, lamports: u64, to: &'a AccountInfo<'info>) {
-        **self.lamports.borrow_mut() -= lamports;
-        **to.lamports.borrow_mut() += lamports;
-    }
+	#[inline(always)]
+	fn send(&'a self, lamports: u64, to: &'a AccountInfo<'info>) {
+		**self.lamports.borrow_mut() -= lamports;
+		**to.lamports.borrow_mut() += lamports;
+	}
 
-    #[inline(always)]
-    fn collect(&'a self, lamports: u64, from: &'a AccountInfo<'info>) -> Result<(), ProgramError> {
-        solana_program::program::invoke(
-            &solana_program::system_instruction::transfer(from.key, self.key, lamports),
-            &[from.clone(), self.clone()],
-        )
-    }
+	#[inline(always)]
+	fn collect(&'a self, lamports: u64, from: &'a AccountInfo<'info>) -> Result<(), ProgramError> {
+		solana_program::program::invoke(
+			&solana_program::system_instruction::transfer(from.key, self.key, lamports),
+			&[from.clone(), self.clone()],
+		)
+	}
 }
 
 impl<'a, 'info> CloseAccount<'a, 'info> for AccountInfo<'info> {
-    fn close(&'a self, to: &'a AccountInfo<'info>) -> Result<(), ProgramError> {
-        // Realloc data to zero.
-        self.realloc(0, true)?;
+	fn close(&'a self, to: &'a AccountInfo<'info>) -> Result<(), ProgramError> {
+		// Realloc data to zero.
+		self.realloc(0, true)?;
 
-        // Return rent lamports.
-        self.send(self.lamports(), to);
+		// Return rent lamports.
+		self.send(self.lamports(), to);
 
-        Ok(())
-    }
+		Ok(())
+	}
 }
